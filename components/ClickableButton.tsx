9e916c6e30e48c1972a346bf50881f3dd8202169
Jayanta Adhikary <jayantaadhikary@protonmail.com>
2023-11-05 00:50:29 +0530

    profile page desing

---
+++ components/ClickableButton.tsx
@@ -0,0 +1,49 @@
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import React from "react";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+type ClickableButtonProps = {
+  text?: string;
+  func?: () => void;
+  imageName?: any;
+};
+
+const ClickableButton = ({ text, func, imageName }: ClickableButtonProps) => {
+  return (
+    <View>
+      <TouchableOpacity>
+        <View style={styles.clickableOption}>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <Ionicons
+                name={imageName}
+                size={24}
+                color="black"
+                style={{ marginRight: 10 }}
+              />
+              <Text style={{ fontSize: 16, fontWeight: "500" }}>{text}</Text>
+            </View>
+            <Ionicons
+              name="chevron-forward-outline"
+              size={24}
+              color="black"
+              style={{ marginLeft: "auto" }}
+            />
+          </View>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default ClickableButton;
+
+const styles = StyleSheet.create({
+  clickableOption: {
+    // backgroundColor: "white",
+    padding: 18,
+    // borderRadius: 12,
+    // marginBottom: 10,
+    // marginTop: 5,
+  },
+});
--- screens/user/Profile.tsx
+++ screens/user/Profile.tsx
@@ -1,8 +1,24 @@
-import { View, Text, StyleSheet, Button } from "react-native";
+import {
+  SafeAreaView,
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Alert,
+  ScrollView,
+} from "react-native";
 import { useEffect, useState } from "react";
 import { signOut } from "firebase/auth";
 import { auth, firestore } from "../../config/firebase";
 import { collection, getDocs, query, where } from "firebase/firestore";
+import { Avatar } from "@rneui/themed";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import ClickableButton from "../../components/ClickableButton";
+
+type UserType = {
+  fullName: string;
+  email: string;
+};
 
 const Profile = () => {
   const [userDetails, setUserDetails] = useState({
@@ -23,23 +39,75 @@ const Profile = () => {
 
       const querySnapshot = getDocs(q).then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
-          setUserDetails(doc.data());
+          setUserDetails(doc.data() as UserType);
         });
       });
     }
   }, []);
 
-  const handleLogout = async () => {
-    await signOut(auth);
+  const handleLogout = () => {
+    Alert.alert("Logout", "Are you sure you want to logout?", [
+      {
+        text: "Yes",
+        onPress: async () => {
+          await signOut(auth);
+        },
+      },
+      {
+        text: "No",
+      },
+    ]);
   };
 
   return (
-    <View style={styles.container}>
-      <Text>Profile</Text>
-      <Text>Current User: {userDetails.fullName}</Text>
-      <Text>Email: {userDetails.email}</Text>
-      <Button title="Logout" onPress={handleLogout} />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "flex-end",
+          marginRight: 10,
+          marginTop: 10,
+        }}
+      >
+        <TouchableOpacity onPress={handleLogout}>
+          <Ionicons name="log-out-outline" size={25} color="black" />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.avatarContainer}>
+        <Avatar
+          size={100}
+          rounded
+          title={userDetails.fullName
+            .split(" ")
+            .map((n) => n[0])
+            .join("")}
+          containerStyle={{ backgroundColor: "#245a51" }}
+        />
+        <Text style={{ marginTop: 25, fontWeight: "700", fontSize: 20 }}>
+          {userDetails.fullName}
+        </Text>
+        <Text style={{ marginTop: 5, fontWeight: "500", fontSize: 16 }}>
+          {userDetails.email}{" "}
+        </Text>
+      </View>
+      <ScrollView
+        style={styles.dataContainer}
+        showsVerticalScrollIndicator={false}
+      >
+        <ClickableButton
+          text="Edit Profile"
+          imageName="person-circle-outline"
+        />
+        <ClickableButton text="My Posts" imageName="list-outline" />
+        <ClickableButton text="Saved Posts" imageName="bookmark-outline" />
+        <ClickableButton text="Settings" imageName="settings-outline" />
+        <ClickableButton text="Help" imageName="help-circle-outline" />
+        <ClickableButton
+          text="Invite a friend"
+          imageName="person-add-outline"
+        />
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
@@ -48,7 +116,23 @@ export default Profile;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  avatarContainer: {
     alignItems: "center",
-    justifyContent: "center",
+    marginTop: 50,
+    marginBottom: 50,
+  },
+  dataContainer: {
+    flex: 1,
+    backgroundColor: "white",
+    borderTopLeftRadius: 60,
+    borderTopRightRadius: 60,
+    padding: 22,
+  },
+  clickableOption: {
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 12,
+    marginBottom: 20,
   },
 });
